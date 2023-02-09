dbc6f1fece8c70c740166020bc2c0f586c1ef557
Nisarg <nisargs2001@gmail.com>
2023-02-09 23:30:43 +0530

    minor change in user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    require: [true, "name is required"],
+    required: [true, "name is required"],
     maxLength: [25, "max length is 25"],
     trim: true,
   },
   email: {
     type: String,
     unique: true,
-    require: [true, "email is required"],
+    required: [true, "email is required"],
   },
 });
 
